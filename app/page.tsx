ec9e0a18dc2b9465180ee017352a5b3e300fc015
AurelienPospieszynski <aurelien.pos@hotmail.fr>
2024-04-16 17:44:14 +0200

    Add spacing component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image';
 import { Header } from './_components/Header';
 import { Hero } from './_components/Hero';
+import { Spacing } from './_components/Spacing';
 
 export default function Home() {
   return (
     <main>
       {/* Header is here. */}
       <Header />
+
+      <Spacing size="md" />
+
       {/* Hero section is here. It contains the profil picture + quick introduction of myself */}
       <Hero />
     </main>
